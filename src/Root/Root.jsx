d948d402c450684f51ee65e64771bcae9aedec05
Jubair <jubair.ahmed7946@gmail.com>
2023-12-14 15:22:11 +0600

    Delete: react animated cursor deleted

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -2,15 +2,13 @@ import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 
-
-
 const Root = () => {
-   
-    return(
-        <div>
-            <Outlet/>
-            <Toaster/>
-            <Footer/>
-        </div>
-    )}
+  return (
+    <div>
+      <Outlet />
+      <Toaster />
+      <Footer />
+    </div>
+  );
+};
 export default Root;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,26 +2,25 @@ import { useEffect } from "react";
 import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Navbar/Navbar";
 import Projects from "../../components/Projects/Projects";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import ScrollToTop from "../../utils/ScrollToTop";
 import Contact from "../../components/Contact/Contact";
 const Home = () => {
-    useEffect(() => {
-        AOS.init();
-      }, []);
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-   
-    return(
-        <div >
-            <Navbar/>
-           <Banner/>
-           <div className="bg-[#050b1f]">
-
-           <Projects/>
-         <ScrollToTop/>
-         <Contact/>
-           </div>
-        </div>
-    )}
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <div className="bg-[#050b1f]">
+        <Projects />
+        <ScrollToTop />
+        <Contact />
+      </div>
+    </div>
+  );
+};
 export default Home;
