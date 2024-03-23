76c3c4a143c672d7d6498bca8fb38025b7df478b
Jubair <jubair.ahmed2838@gmail.com>
2024-03-23 20:31:32 +0600

    Add: embed youtube video of my video resume

--- src/components/VideoResume/VideoResume.jsx
+++ src/components/VideoResume/VideoResume.jsx
@@ -1,7 +1,7 @@
-const VideoResume = () => {
+const VideoResume  = () => {
     return(
         <div>
-             <p> HELLO I Am VideoResume </p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/hV93s5Av6Go?si=-c-T-PZRvBTWW52d" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     )}
-export default VideoResume;
+export default VideoResume ;
